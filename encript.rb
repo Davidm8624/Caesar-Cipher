d8543eaca3de83c2ce555321b285bf93d1b84fcb
Davidm8624 <davidm8624@gmail.com>
2023-04-07 10:50:21 -0700

    works, test it out

--- encript.rb
+++ encript.rb
@@ -1,63 +1,38 @@
-insecure = gets.chomp.split("")
-temp = ""
-shift = gets.chomp
-secure = ""
+def caesar_cipher(string,shift_factor)
 
-alphabet = {
-  a: 0,
-  b: 1,
-  c: 2,
-  d: 3,
-  e: 4,
-  f: 5,
-  g: 6,
-  h: 7,
-  i: 8,
-  j: 9,
-  k: 10,
-  l: 11,
-  m: 12,
-  n: 13,
-  o: 14,
-  p: 15,
-  q: 16,
-  r: 17,
-  s: 18,
-  t: 19,
-  v: 20,
-  w: 21,
-  x: 22,
-  y: 23,
-  z: 24,
-}
+    # Split string into character array, get ascii value for each character, this is the ascii numers for all the characters
+    # 65 - 90 for A-B
+    # 97 - 122 for a-b
+    asciis = string.split("").map { |e| e.ord }
 
-i = 0
-insecure.length.times do
-  temp = temp + insecure[i]
-  i = i + 1
+    shifted = asciis.map do |ascii|
+        # check if character is aplhabetic, yes - do shift , no - no shift
+        if ascii.between?(65,90)
+            # logic for wrap around
+            if shift_factor.positive?
+                ascii + shift_factor > 90 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+            else
+                ascii + shift_factor < 65 ? (ascii + shift_factor) + 26 : ascii + shift_factor
+            end
+        elsif ascii.between?(97,122)
+            if shift_factor.positive?
+                ascii + shift_factor > 122 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+            else
+                ascii + shift_factor < 97 ? (ascii + shift_factor) + 26 : ascii + shift_factor
+            end
+        else
+            ascii
+        end
+    end
+
+    shifted_string = shifted.map! { |a| a.chr }.join
+    p shifted_string
 end
-# what this does is for each character in insecure it takes the first letter, does something to it, saves it to temp then moves on to the next letter
 
+print "Enter the string to encode : "
+string = gets.chomp
 
-#hey! me! in the future. look more at the .each method
+print "Enter the shift factor for the string : "
+shift_factor = gets.chomp.to_i
 
-
-
-puts insecure, shift
-
-
-
-
-
-
-=begin
-  
-idea of how this will work:
-
-it asks user for word and will seperate all the characters.
-it will then look for the character in the alphabet hash.
-then when it gets a number from the user (postivie or nevative) it will find the number value that is affiliated with that key
-using the number that it gets from the user it will add or subtract the number from the value of the key
-it will add that letter to the "encripted" and move onto the next letter returning the whole string once it is done encripting
-  
-=end
+caesar_cipher(string,shift_factor)

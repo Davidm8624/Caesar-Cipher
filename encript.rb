39fe288bc452eb37688561816e81254157a1b8a7
davidm8624 <davidm8624@gmail.com>
2024-06-17 17:28:50 -0700

    misc

--- encript.rb
+++ encript.rb
@@ -1,32 +1,31 @@
-def caesar_cipher(string,shift_factor)
+def caesar_cipher(string, shift_factor)
+  # Split string into character array, get ascii value for each character, this is the ascii numers for all the characters
+  # 65 - 90 for A-B
+  # 97 - 122 for a-b
+  asciis = string.split("").map { |e| e.ord }
 
-    # Split string into character array, get ascii value for each character, this is the ascii numers for all the characters
-    # 65 - 90 for A-B
-    # 97 - 122 for a-b
-    asciis = string.split("").map { |e| e.ord }
-
-    shifted = asciis.map do |ascii|
-        # check if character is aplhabetic, yes - do shift , no - no shift
-        if ascii.between?(65,90)
-            # logic for wrap around
-            if shift_factor.positive?
-                ascii + shift_factor > 90 ? (ascii + shift_factor) - 26 : ascii + shift_factor
-            else
-                ascii + shift_factor < 65 ? (ascii + shift_factor) + 26 : ascii + shift_factor
-            end
-        elsif ascii.between?(97,122)
-            if shift_factor.positive?
-                ascii + shift_factor > 122 ? (ascii + shift_factor) - 26 : ascii + shift_factor
-            else
-                ascii + shift_factor < 97 ? (ascii + shift_factor) + 26 : ascii + shift_factor
-            end
-        else
-            ascii
-        end
+  shifted = asciis.map do |ascii|
+    # check if character is aplhabetic, yes - do shift , no - no shift
+    if ascii.between?(65, 90)
+      # logic for wrap around
+      if shift_factor.positive?
+        ascii + shift_factor > 90 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+      else
+        ascii + shift_factor < 65 ? (ascii + shift_factor) + 26 : ascii + shift_factor
+      end
+    elsif ascii.between?(97, 122)
+      if shift_factor.positive?
+        ascii + shift_factor > 122 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+      else
+        ascii + shift_factor < 97 ? (ascii + shift_factor) + 26 : ascii + shift_factor
+      end
+    else
+      ascii
     end
+  end
 
-    shifted_string = shifted.map! { |a| a.chr }.join
-    p shifted_string
+  shifted_string = shifted.map! { |a| a.chr }.join
+  p shifted_string
 end
 
 print "Enter the string to encode : "
@@ -35,4 +34,4 @@ string = gets.chomp
 print "Enter the shift factor for the string : "
 shift_factor = gets.chomp.to_i
 
-caesar_cipher(string,shift_factor)
+caesar_cipher(string, shift_factor)
